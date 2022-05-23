e70096ed2117ec4f1a3eb4bb6bbea51ecefc4925
NyRakath <soncor23@gmail.com>
2022-05-22 18:28:39 -0700

    FIX

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
 def add_arg(argv):
     n = len(argv) - 1
     if n == 0:
@@ -11,7 +14,3 @@ def add_arg(argv):
             add += int(argv[i])
             i += 1
         print("{:d}".format(add))
-
-if __name__ == "__main__":
-    import sys
-    add_arg(sys.argv)

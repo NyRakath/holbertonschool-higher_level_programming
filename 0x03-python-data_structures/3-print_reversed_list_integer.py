f56a86a6c5d9d535491855798879c0be454da855
NyRakath <soncor23@gmail.com>
2022-05-22 21:27:15 -0700

    reverse & new

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return
+    size = len(my_list) - 1
+    while size >= 0:
+        print("{:d}".format(my_list[size]))
+        size -= 1

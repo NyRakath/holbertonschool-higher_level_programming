8d9f7202aeaee6761fe14bd1a9f0ccf32510e13a
NyRakath <soncor23@gmail.com>
2022-05-23 19:11:21 -0700

    Num Roman convert

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    sum = 0
-    if type(roman_string) is str and roman_string is not "":
-        decode = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                  'C': 100, 'D': 500, 'M': 1000}
-        carry = [roman_string[0], 0]
-        for c in roman_string:
-            if c not in decode:
-                return 0
-            if carry[0] == c:
-                carry[1] += decode[c]
-            elif decode[carry[0]] < decode[c]:
-                sum -= carry[1]
-                carry[0] = c
-                carry[1] = decode[c]
-            else:
-                sum += carry[1]
-                carry[0] = c
-                carry[1] = decode[c]
-        sum += carry[1]
-    return sum
+    if not roman_string or type(roman_string) is not str:
+        return 0
+
+    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+
+    int_num = []
+    for letter in roman_string:
+        for symbol in roman_num.keys():
+            if letter == symbol:
+                int_num.append(roman_num.get(symbol))
+
+    last = len(int_num)
+    if last >= 1:
+        for i in range(last - 1):
+            if int_num[i] < int_num[i + 1]:
+                int_num[i] *= -1
+
+    return sum(int_num)
+

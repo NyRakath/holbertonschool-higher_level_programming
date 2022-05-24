0be9a710fdcade4528415396a3be25afdb5332fb
NyRakath <soncor23@gmail.com>
2022-05-23 18:42:59 -0700

    Fix 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    new_dict = {}
-    for k in my_dict:
-        new_dict[k] = 2 * my_dict[k]
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for key in new_dict.keys():
+        elm = new_dict.get(key, None) * 2
+        new_dict.update({key: elm})
+
     return new_dict

d96334808a6ba429c8577fb0aaa0a4fc703268ae
NyRakath <soncor23@gmail.com>
2022-05-24 21:12:31 -0700

    update fix math area

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
+""" Raising the power of beating the bytecode. """
 import math
-"""
-This is a Magic Class that reverses a given Class's bytecode
-"""
 
 
 class MagicClass:
+
+    """ Define the radius attribute """
     def __init__(self, radius=0):
         self.__radius = 0
-        if (type(radius) is not int) and (type(radius) is not float):
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
+    """ Set the power of radius and math.pi """
     def area(self):
         return (self.__radius ** 2) * math.pi
 
+    """ Circum the radius """
     def circumference(self):
-        return (2 * math.pi) * self.__radius
+        return 2 * math.pi * self.__radius
